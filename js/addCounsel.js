182783518a4740b6544747547cc37e51655fe782
YonghoChoi <yongho1037@gmail.com>
2020-09-13 21:32:04 +0900

    상담 추가 시 현장 정보는 기록된 현장 이름 선택할 수 있도록 개선

--- js/addCounsel.js
+++ js/addCounsel.js
@@ -1,6 +1,7 @@
 // 모달 팝업 오픈 스크립트
 const counselModal = document.querySelector('.add_counsel');
 const addCounselBtn = document.querySelectorAll('aside .add_btn');
+loadPlaceNames();
 
 for (let i = 0; i < addCounselBtn.length; i++) {
     addCounselBtn[i].addEventListener('click', counselMoadalOpen)
@@ -12,4 +13,27 @@ function counselMoadalOpen() {
     } else {
         counselModal.classList.add('on')
     }
+}
+
+function loadPlaceNames() {
+    $.ajax({
+        type: "GET",
+        url: `https://3min.yonghochoi.com/api/places/name`,
+        data: {},
+        success: function (response) {
+            if (!response.data) {
+                console.log("현장 정보가 존재하지 않습니다.");
+                return;
+            }
+            
+            const placeNameSelect = document.getElementById("counsel_placename")
+            for(placeName of response.data) {
+                const op = new Option();
+                op.value = placeName;
+                op.text = placeName;
+                
+                placeNameSelect.add(op);
+            }
+        }
+    });
 }
